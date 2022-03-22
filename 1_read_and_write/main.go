6c801d15ffed44741ddc052321d4d32d3d6bea8c
Katsuragawa <andre.katsuragawa@gmail.com>
2022-03-22 18:02:57 -0300

    Handle basic http requests

---
+++ 1_read_and_write/main.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+)
+
+func main() {
+	http.HandleFunc("/", func(rw http.ResponseWriter, r *http.Request) {
+		log.Println("Hey")
+		d, err := ioutil.ReadAll(r.Body)
+
+		if err != nil {
+			http.Error(rw, "some error", http.StatusBadRequest) // same stuff
+			// rw.WriteHeader(http.StatusBadRequest)
+			// rw.Write([]byte("Some error"))
+			return
+		}
+
+		fmt.Fprintf(rw, "Your data %s", d)
+	})
+
+	http.HandleFunc("/goodbye", func(http.ResponseWriter, *http.Request) {
+		log.Println("Bye")
+	})
+
+	http.ListenAndServe(":9090", nil)
+}
